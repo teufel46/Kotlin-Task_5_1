90f9a88461e0f1f8d739f2a8a8e372e83666cf5f
Polina Novikova <9104785163@mail.ru>
2022-01-12 14:27:35 +0300

    additing all fiels

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-/*data class Comments(
+data class Comments(
     val count : Int,
     val canPost : Boolean,
     val groupsCanPost : Boolean,
@@ -36,17 +36,17 @@ data class Donut(
     val canPublishFreeCopy : Boolean,
     val editMode : String
 )
-*/
+
 data class Post(
     var id : Long = 0,
     val ownerId : Long,
     var fromId : Long,
- /*   val createdBy : Long,
-    val date : Long,
-    val text : String,
-    val replyOwnerId : Long,
-    val replyPostId : Long,
-    val friendsOnly : Boolean,
+    var createdBy : Long,
+    var date : Long,
+    var text : String,
+    var replyOwnerId : Long,
+    var replyPostId : Long,
+    var friendsOnly : Boolean,
     val comments : Comments,
     val copyright : Copyright,
     val likes : Likes,
@@ -62,8 +62,6 @@ data class Post(
     val isFavorite : Boolean,
     val donut : Donut,
     val postponedId : Long
-
-  */
 )
 
 object WallService {
@@ -77,13 +75,10 @@ object WallService {
         return posts.last()
     }
 
-    fun update(post: Post) : Boolean {
-        println("====WallService.update===========================")
-        val idFind: Long = post.id
-        for (post in posts) {
-            if (post.id == idFind) {
-                post.fromId = post.fromId + 1
-
+    fun update(postFind: Post) : Boolean {
+        for ((index, post) in posts.withIndex()){
+            if (post.id == postFind.id) {
+                posts[index] = postFind.copy(ownerId = post.ownerId, date = post.date)
                 return true
             }
         }
@@ -91,8 +86,6 @@ object WallService {
     }
 
     fun print(){
-        println("====WallService.print============================")
-        println("  Posts")
         for (post in posts){
             println(post)
         }
@@ -105,16 +98,118 @@ object WallService {
 
 fun main(args: Array<String>) {
 
-    val post1 = Post(123, 12345689, 12345689)
-    val post2 = Post(234, 12345665, 12345665)
-    val post3 = Post(789, 12345670, 12345555)
+    val post1 = Post(
+        123,
+        12345689,
+        12345689,
+        111111,
+        1460041200,
+        "Текст поста",
+        12345689,
+        6765,
+        true,
+        Comments(123, true, false, false,true),
+        Copyright(23452784, "dfdfdsf","Текст","тип"),
+        Likes(4873, true, true,true),
+        Reposts(23, false),
+        Views(21),
+        "post", //post, copy, reply, postpone, suggest.
+        12786,
+        true,
+        true,
+        false,
+        true,
+        false,
+        false,
+        Donut(true, 223,"placeholder", true, "all"),
+        1
+    )
+    val post2 = Post(
+        234,
+        12345665,
+        12345665,
+        111111,
+        1460041200,
+        "Текст поста",
+        12345689,
+        6765,
+        true,
+        Comments(123, true, false, false,true),
+        Copyright(23452784, "dfdfdsf","Текст","тип"),
+        Likes(4873, true, true,true),
+        Reposts(23, false),
+        Views(21),
+        "post", //post, copy, reply, postpone, suggest.
+        12786,
+        true,
+        true,
+        false,
+        true,
+        false,
+        false,
+        Donut(true, 223,"placeholder", true, "all"),
+        1
+    )
+    val post3 = Post(
+        789,
+        12345670,
+        12345555,
+        111111,
+        1460041200,
+        "Текст поста",
+        12345689,
+        6765,
+        false,
+        Comments(123, true, false, false,true),
+        Copyright(23452784, "dfdfdsf","Текст","тип"),
+        Likes(4873, true, true,true),
+        Reposts(23, false),
+        Views(21),
+        "post", //post, copy, reply, postpone, suggest.
+        12786,
+        true,
+        true,
+        false,
+        true,
+        false,
+        false,
+        Donut(true, 223,"placeholder", true, "all"),
+        1
+    )
+
+    val post4 = Post(
+        2,
+        99999999,
+        99999999,
+        99999999,
+        1460041200,
+        "Update",
+        99999999,
+        99999999,
+        true,
+        Comments(99999999, true, false, false,true),
+        Copyright(99999999, "Update","Update","Update"),
+        Likes(99999999, true, true,true),
+        Reposts(99999999, false),
+        Views(99999999),
+        "post", //post, copy, reply, postpone, suggest.
+        99999999,
+        true,
+        true,
+        false,
+        true,
+        false,
+        false,
+        Donut(true, 99999999,"Update", true, "Update"),
+        1
+    )
 
     WallService.add(post1)
     WallService.add(post2)
 
     WallService.print()
 
-    if (WallService.update(post2)) WallService.print()
+    if (WallService.update(post4)) WallService.print()
     if (WallService.update(post3)) WallService.print()
 
 
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -7,10 +7,111 @@ class MainKtTest {
     @Test
     fun updateExisting() {
         val service = WallService
-        service.add(Post(0, 12345689, 12345689))
-        service.add(Post(0, 12345665, 12345665))
-        service.add(Post(0, 12345670, 12345555))
-        val update = Post(2, 12345665, 12345665)
+        val post1 = Post(
+            123,
+            12345689,
+            12345689,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            true,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        )
+        val post2 = Post(
+            234,
+            12345665,
+            12345665,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            true,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        )
+        val post3 = Post(
+            789,
+            12345670,
+            12345555,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            false,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        )
+        val update = Post(
+            2,
+            99999999,
+            99999999,
+            99999999,
+            1460041200,
+            "Update",
+            99999999,
+            99999999,
+            true,
+            Comments(99999999, true, false, false,true),
+            Copyright(99999999, "Update","Update","Update"),
+            Likes(99999999, true, true,true),
+            Reposts(99999999, false),
+            Views(99999999),
+            "post", //post, copy, reply, postpone, suggest.
+            99999999,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 99999999,"Update", true, "Update"),
+            1
+        )
+
         val result = service.update(update)
         assertTrue(result)
     }
@@ -20,13 +121,110 @@ class MainKtTest {
         // создаём целевой сервис
         val service = WallService
         // заполняем несколькими постами
-        service.add(Post(0, 12345689, 12345689))
-        service.add(Post(0, 12345665, 12345665))
-        service.add(Post(0, 12345670, 12345555))
-
-
-        // создаём информацию об обновлении
-        val update = Post(234, 12345665, 12345665)
+        val post1 = Post(
+            123,
+            12345689,
+            12345689,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            true,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        )
+        val post2 = Post(
+            234,
+            12345665,
+            12345665,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            true,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        )
+        val post3 = Post(
+            789,
+            12345670,
+            12345555,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            false,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        )
+        val update = Post(
+            2,
+            99999999,
+            99999999,
+            99999999,
+            1460041200,
+            "Update",
+            99999999,
+            99999999,
+            true,
+            Comments(99999999, true, false, false,true),
+            Copyright(99999999, "Update","Update","Update"),
+            Likes(99999999, true, true,true),
+            Reposts(99999999, false),
+            Views(99999999),
+            "post", //post, copy, reply, postpone, suggest.
+            99999999,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 99999999,"Update", true, "Update"),
+            1
+        )
 
         // выполняем целевое действие
         val result = service.update(update)
@@ -38,8 +236,32 @@ class MainKtTest {
     @Test
     fun add() {
         val service = WallService
-        service.add(Post(0, 12345689, 12345689))
+        service.add(Post(
+            123,
+            12345689,
+            12345689,
+            111111,
+            1460041200,
+            "Текст поста",
+            12345689,
+            6765,
+            true,
+            Comments(123, true, false, false,true),
+            Copyright(23452784, "dfdfdsf","Текст","тип"),
+            Likes(4873, true, true,true),
+            Reposts(23, false),
+            Views(21),
+            "post", //post, copy, reply, postpone, suggest.
+            12786,
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            Donut(true, 223,"placeholder", true, "all"),
+            1
+        ))
         assertTrue(service.posts[0].id > 0)
     }
-
 }
